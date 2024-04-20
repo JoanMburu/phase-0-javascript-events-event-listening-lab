f4bae4c6bd3db966249da31a33d08238ed826fe5
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-20 18:39:46 +0300

    added an event listener

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+// Will handle the click
+function handleClick(input){
+    return alert ('I created an event listening alert')
+    console.log(input);
 }
+// Grabs button
+function addingEventListener() {
+    let input = document.getElementById('button')
+    input.addEventListener('click', handleClick)
+}
+addingEventListener();
